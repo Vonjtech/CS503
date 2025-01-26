94622ca52b9a1cc482941f7cec4700daa46e959b
Ibrahim Edward Vonjoe <vonjoeibrahime@gmail.com>
2025-01-25 20:13:11 -0500

    Update stringfun.c
    fun file v_002

--- Assignments/Assignment2/stringfun.c
+++ Assignments/Assignment2/stringfun.c
@@ -1,117 +1,173 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-
 
 #define BUFFER_SZ 50
 
-//prototypes
-void usage(char *);
-void print_buff(char *, int);
-int  setup_buff(char *, char *, int);
+// Function prototypes
+int setup_buff(char *buff, const char *user_str, int buff_size);
+int count_words(const char *buff, int buff_size, int str_len);
+void reverse_string(char *buff, int str_len);
+void print_words(const char *buff, int str_len);
+void print_help();
+void print_buff(const char *buff, int buff_size);
 
-//prototypes for functions to handle required functionality
-int  count_words(char *, int, int);
-//add additional prototypes here
-
-
-int setup_buff(char *buff, char *user_str, int len){
-    //TODO: #4:  Implement the setup buff as per the directions
-    return 0; //for now just so the code compiles. 
-}
-
-void print_buff(char *buff, int len){
-    printf("Buffer:  ");
-    for (int i=0; i<len; i++){
-        putchar(*(buff+i));
-    }
-    putchar('\n');
-}
-
-void usage(char *exename){
-    printf("usage: %s [-h|c|r|w|x] \"string\" [other args]\n", exename);
-
-}
-
-int count_words(char *buff, int len, int str_len){
-    //YOU MUST IMPLEMENT
-    return 0;
-}
-
-//ADD OTHER HELPER FUNCTIONS HERE FOR OTHER REQUIRED PROGRAM OPTIONS
-
-int main(int argc, char *argv[]){
-
-    char *buff;             //placehoder for the internal buffer
-    char *input_string;     //holds the string provided by the user on cmd line
-    char opt;               //used to capture user option from cmd line
-    int  rc;                //used for return codes
-    int  user_str_len;      //length of user supplied string
-
-    //TODO:  #1. WHY IS THIS SAFE, aka what if arv[1] does not exist?
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
-    if ((argc < 2) || (*argv[1] != '-')){
-        usage(argv[0]);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "error: Incorrect usage. Use -h for help.\n");
         exit(1);
     }
 
-    opt = (char)*(argv[1]+1);   //get the option flag
+    char *option = argv[1];
+    char *user_str = argv[2];
 
-    //handle the help flag and then exit normally
-    if (opt == 'h'){
-        usage(argv[0]);
-        exit(0);
-    }
-
-    //WE NOW WILL HANDLE THE REQUIRED OPERATIONS
-
-    //TODO:  #2 Document the purpose of the if statement below
-    //      PLACE A COMMENT BLOCK HERE EXPLAINING
-    if (argc < 3){
-        usage(argv[0]);
-        exit(1);
-    }
-
-    input_string = argv[2]; //capture the user input string
-
-    //TODO:  #3 Allocate space for the buffer using malloc and
-    //          handle error if malloc fails by exiting with a 
-    //          return code of 99
-    // CODE GOES HERE FOR #3
-
-
-    user_str_len = setup_buff(buff, input_string, BUFFER_SZ);     //see todos
-    if (user_str_len < 0){
-        printf("Error setting up buffer, error = %d", user_str_len);
+    char *buff = (char *)malloc(BUFFER_SZ);
+    if (!buff) {
+        fprintf(stderr, "error: Memory allocation failed.\n");
         exit(2);
     }
 
-    switch (opt){
-        case 'c':
-            rc = count_words(buff, BUFFER_SZ, user_str_len);  //you need to implement
-            if (rc < 0){
-                printf("Error counting words, rc = %d", rc);
-                exit(2);
-            }
-            printf("Word Count: %d\n", rc);
-            break;
-
-        //TODO:  #5 Implement the other cases for 'r' and 'w' by extending
-        //       the case statement options
-        default:
-            usage(argv[0]);
-            exit(1);
+    int str_len = setup_buff(buff, user_str, BUFFER_SZ);
+    if (str_len < 0) {
+        fprintf(stderr, "error: Provided input string is too long.\n");
+        free(buff);
+        exit(3);
     }
 
-    //TODO:  #6 Dont forget to free your buffer before exiting
-    print_buff(buff,BUFFER_SZ);
-    exit(0);
+    if (option[0] == '-' && option[1] != '\0') {
+        switch (option[1]) {
+            case 'h':
+                print_help();
+                break;
+            case 'c':
+                printf("Word Count: %d\n", count_words(buff, BUFFER_SZ, str_len));
+                break;
+            case 'r':
+                reverse_string(buff, str_len);
+                printf("Reversed String: %.*s\n", str_len, buff);
+                break;
+            case 'w':
+                print_words(buff, str_len);
+                break;
+            case 'x':
+                if (argc != 5) {
+                    fprintf(stderr, "error: Incorrect usage. Provide two additional strings.\n");
+                    free(buff);
+                    exit(3);
+                }
+                printf("Not Implemented!\n");
+                break;
+            default:
+                fprintf(stderr, "error: Unknown option. Use -h for help.\n");
+                free(buff);
+                exit(1);
+        }
+    } else {
+        fprintf(stderr, "error: Invalid command format.\n");
+        free(buff);
+        exit(1);
+    }
+
+    print_buff(buff, BUFFER_SZ);
+    free(buff);
+    return 0;
 }
 
-//TODO:  #7  Notice all of the helper functions provided in the 
-//          starter take both the buffer as well as the length.  Why
-//          do you think providing both the pointer and the length
-//          is a good practice, after all we know from main() that 
-//          the buff variable will have exactly 50 bytes?
-//  
-//          PLACE YOUR ANSWER HERE
+int setup_buff(char *buff, const char *user_str, int buff_size) {
+    char *b = buff;
+    const char *s = user_str;
+    int count = 0;
+    int space_flag = 0;
+
+    while (*s != '\0' && count < buff_size) {
+        if (*s == ' ' || *s == '\t') {
+            if (!space_flag && count > 0) {
+                *b++ = ' ';
+                count++;
+                space_flag = 1;
+            }
+        } else {
+            *b++ = *s;
+            count++;
+            space_flag = 0;
+        }
+        s++;
+    }
+
+    if (count >= buff_size)
+        return -1; // Buffer overflow
+
+    while (count < buff_size) {
+        *b++ = '.';
+        count++;
+    }
+
+    return count;
+}
+
+int count_words(const char *buff, int buff_size, int str_len) {
+    int count = 0;
+    int in_word = 0;
+    const char *p = buff;
+
+    for (int i = 0; i < str_len; i++) {
+        if (*p != ' ' && *p != '.') {
+            if (!in_word) {
+                count++;
+                in_word = 1;
+            }
+        } else {
+            in_word = 0;
+        }
+        p++;
+    }
+    return count;
+}
+
+void reverse_string(char *buff, int str_len) {
+    char *start = buff;
+    char *end = buff + str_len - 1;
+
+    while (start < end) {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+void print_words(const char *buff, int str_len) {
+    printf("Word Print\n----------\n");
+    int count = 1;
+    const char *p = buff;
+    const char *start = NULL;
+
+    for (int i = 0; i < str_len; i++) {
+        if (*p != ' ' && *p != '.') {
+            if (start == NULL)
+                start = p;
+        } else {
+            if (start != NULL) {
+                printf("%d. %.*s (%ld)\n", count++, (int)(p - start), start, p - start);
+                start = NULL;
+            }
+        }
+        p++;
+    }
+
+    if (start != NULL)
+        printf("%d. %.*s (%ld)\n", count, (int)(p - start), start, p - start);
+}
+
+void print_help() {
+    printf("Usage: stringfun -[h|c|r|w|x] \"sample string\" [other_options]\n");
+    printf("  -h    Prints help\n");
+    printf("  -c    Counts words in the string\n");
+    printf("  -r    Reverses the string\n");
+    printf("  -w    Prints words with lengths\n");
+    printf("  -x    Replace first occurrence of a word with another\n");
+}
+
+void print_buff(const char *buff, int buff_size) {
+    printf("Buffer: %.*s\n", buff_size, buff);
+}
