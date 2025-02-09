00abef510db1ad9cfd7997c4022da4eabd6669e9
Ibrahim Edward Vonjoe <vonjoeibrahime@gmail.com>
2025-02-08 23:02:46 -0500

    Update dshlib.c
    Lib

--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -5,35 +5,53 @@
 
 #include "dshlib.h"
 
-/*
- *  build_cmd_list
- *    cmd_line:     the command line from the user
- *    clist *:      pointer to clist structure to be populated
- *
- *  This function builds the command_list_t structure passed by the caller
- *  It does this by first splitting the cmd_line into commands by spltting
- *  the string based on any pipe characters '|'.  It then traverses each
- *  command.  For each command (a substring of cmd_line), it then parses
- *  that command by taking the first token as the executable name, and
- *  then the remaining tokens as the arguments.
- *
- *  NOTE your implementation should be able to handle properly removing
- *  leading and trailing spaces!
- *
- *  errors returned:
- *
- *    OK:                      No Error
- *    ERR_TOO_MANY_COMMANDS:   There is a limit of CMD_MAX (see dshlib.h)
- *                             commands.
- *    ERR_CMD_OR_ARGS_TOO_BIG: One of the commands provided by the user
- *                             was larger than allowed, either the
- *                             executable name, or the arg string.
- *
- *  Standard Library Functions You Might Want To Consider Using
- *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
- */
+
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+    if (strlen(cmd_line) == 0)
+    {
+        return WARN_NO_CMDS; // Empty input
+    }
+
+    // Initialization
+    clist->num = 0;
+    memset(clist->commands, 0, sizeof(clist->commands));
+
+    char *saveptr; 
+    char *token = strtok_r(cmd_line, PIPE_STRING, &saveptr);
+
+    while (token != NULL)
+    {
+        if (clist->num >= CMD_MAX)
+        {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        
+        while (isspace((unsigned char)*token))
+            token++;
+
+     
+        char *end = token + strlen(token) - 1;
+        while (end > token && isspace((unsigned char)*end))
+            end--;
+        *(end + 1) = '\0';
+
+        
+        char *cmd_name = strtok(token, " ");  
+        char *cmd_args = strtok(NULL, "");    
+
+        if (cmd_name != NULL)
+        {
+            strncpy(clist->commands[clist->num].exe, cmd_name, EXE_MAX - 1);
+            if (cmd_args != NULL)
+            {
+                strncpy(clist->commands[clist->num].args, cmd_args, ARG_MAX - 1);
+            }
+            clist->num++;
+        }
+
+        token = strtok_r(NULL, PIPE_STRING, &saveptr);  
+    return OK;
+    }
 }
