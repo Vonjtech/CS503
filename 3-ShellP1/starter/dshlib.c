13337c31e561d2312bc5d02a07a8aca892a67b8b
Ibrahim <vonjoeibrahim@gmail.com>
2025-02-16 12:45:26 -0500

    Forcing update to include 4-ShellP2

--- 3-ShellP1/starter/dshlib.c
+++ 3-ShellP1/starter/dshlib.c
@@ -34,53 +34,6 @@
  */
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    if (cmd_line == NULL || strlen(cmd_line) == 0)
-        return WARN_NO_CMDS;
-
-    memset(clist, 0, sizeof(command_list_t));
-    char *token = strtok(cmd_line, PIPE_STRING);
-
-    while (token && clist->num < CMD_MAX)
-    {
-        while (*token == SPACE_CHAR)
-            token++;
-        char *end = token + strlen(token) - 1;
-        while (end > token && *end == SPACE_CHAR)
-            *end-- = '\0';
-
-        if (strlen(token) >= EXE_MAX + ARG_MAX)
-        {
-            return ERR_CMD_OR_ARGS_TOO_BIG;
-        }
-
-        char *arg_start = strchr(token, SPACE_CHAR);
-        if (arg_start)
-        {
-            *arg_start = '\0';
-            strcpy(clist->commands[clist->num].exe, token);
-            strcpy(clist->commands[clist->num].args, arg_start + 1);
-        }
-        else
-        {
-            strcpy(clist->commands[clist->num].exe, token);
-        }
-
-        clist->num++;
-        token = strtok(NULL, PIPE_STRING);
-    }
-
-    return (clist->num > 0) ? OK : WARN_NO_CMDS;
+    printf(M_NOT_IMPL);
+    return EXIT_NOT_IMPL;
 }
-
-// void trim_spaces(char *str)
-// {
-//     char *end;
-//     while (isspace((unsigned char)*str))
-//         str++; // Trim leading spaces
-//     if (*str == 0)
-//         return;
-//     end = str + strlen(str) - 1;
-//     while (end > str && isspace((unsigned char)*end))
-//         end--; // Trim trailing spaces
-//     *(end + 1) = '\0';
-// }
